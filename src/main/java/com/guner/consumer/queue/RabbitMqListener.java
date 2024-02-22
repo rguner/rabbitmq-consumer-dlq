c060d19b3a3cf5e808056d481df1f31bf497e4f2
Ramazan Guner <ramazan.guner@haud.com>
2024-02-22 14:12:50 +0300

    dlq

--- src/main/java/com/guner/consumer/queue/RabbitMqListener.java
+++ src/main/java/com/guner/consumer/queue/RabbitMqListener.java
@@ -22,8 +22,9 @@ public class RabbitMqListener {
     private final ChargingRecordService chargingRecordService;
 
 
-    // NACK with throwing Exception
+    // NACK with throwing Exception, it works if MessageNotSuitableException messages goes to DLQ
     //@RabbitListener(queues = "${single-consumer.queue.name.single-queue}", containerFactory = "rabbitListenerContainerFactory")
+   /*
     @RabbitListener(queues = "${single-consumer.queue.name.single-queue}")
     public void listenMessage(ChargingRecord chargingRecord) {
         log.debug("Charging Message Received, thread: {}", Thread.currentThread().getName());
@@ -33,9 +34,9 @@ public class RabbitMqListener {
         }
         chargingRecordService.createChargingRecord(chargingRecord);
     }
+*/
 
 
-    /*
     @RabbitListener(queues = "${single-consumer.queue.name.single-queue}", ackMode = "MANUAL")
     public void listenWithSpringMessage(org.springframework.messaging.Message<ChargingRecord> messageChargingRecord,
                                              Channel channel,
@@ -44,8 +45,8 @@ public class RabbitMqListener {
         if (messageChargingRecord.getPayload().getSourceGsm().endsWith("0")) {
             log.error("Charging Message Source Gsm ends with 0, NACK");
             try {
-                //channel.basicNack(deliveryTag, false, true); // requeue
-                channel.basicNack(deliveryTag, false, false); // no requeue
+                //channel.basicNack(deliveryTag, false, true); // requeue, it requeues to the same queue. Not to DLQ
+                channel.basicNack(deliveryTag, false, false); // no requeue, it sends to DLQ
             } catch (Exception e) {
                 log.error("Error while acknowledging message", e);
             }
@@ -59,6 +60,6 @@ public class RabbitMqListener {
         }
     }
 
-     */
+
 
 }
